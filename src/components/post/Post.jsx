c210fa3dde3de55c3920fea697c4abfd13bb041f
leewebstechnologies <aliuajibade317@gmail.com>
2023-02-20 21:58:48 -0800

    blog

--- src/components/post/Post.jsx
+++ src/components/post/Post.jsx
@@ -1,34 +1,26 @@
 import "./post.css";
+import { Link } from "react-router-dom";
 
-const Post = ({post}) => {
+const Post = ({ post }) => {
   return (
     <div className="post">
-      <img
-        className="postImg"
-        src="https://images.pexels.com/photos/6685428/pexels-photo-6685428.jpeg"
-        alt=""
-      />
+      {post.photo && <img className="postImg" src={post.photo} alt="" />}
+
       <div className="postInfo">
         <div className="postCategories">
-          <span className="postCategory">Music </span>
-          <span className="postCategory">Life</span>
+          {post.categories.map((c) => (
+            <span className="postCategory">{c.name}</span>
+          ))}
         </div>
-        <span className="postTitle">{post.title}</span>
+        <Link to={`/post/${post._id}`}>
+          <span className="postTitle">{post.title}</span>
+        </Link>
         <hr />
-        <span className="postDate">1 hour ago</span>
+        <span className="postDate">
+          {new Date(post.createdAt).toDateString()}
+        </span>
       </div>
-      <p className="postDesc">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate, eum
-        ipsum. Quibusdam nam dignissimos totam accusantium debitis sit rem ex
-        facere provident consequatur et dolor reprehenderit magni non, nostrum
-        placeat! Lorem ipsum dolor sit amet consectetur adipisicing elit.
-        Voluptate, eum ipsum. Quibusdam nam dignissimos totam accusantium
-        debitis sit rem ex facere provident consequatur et dolor reprehenderit
-        magni non, nostrum placeat! Lorem ipsum dolor sit amet consectetur
-        adipisicing elit. Voluptate, eum ipsum. Quibusdam nam dignissimos totam
-        accusantium debitis sit rem ex facere provident consequatur et dolor
-        reprehenderit magni non, nostrum placeat!
-      </p>
+      <p className="postDesc">{post.desc}</p>
     </div>
   );
 };
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -4,17 +4,19 @@ import Posts from "../../components/posts/Posts";
 import Sidebar from "../../components/sidebar/Sidebar";
 import "./home.css";
 import axios from "axios";
+import { useLocation } from "react-router";
 
-const Home = () => {
+export default function Home() {
   const [posts, setPosts] = useState([]);
+  const { search } = useLocation();
 
   useEffect(() => {
     const fetchPosts = async () => {
-      const res = await axios.get("/posts");
+      const res = await axios.get("/posts" + search);
       setPosts(res.data);
     };
     fetchPosts();
-  }, []);
+  }, [search]);
   return (
     <>
       <Header />
@@ -24,6 +26,34 @@ const Home = () => {
       </div>
     </>
   );
-};
+}
 
-export default Home;
+// import { useEffect, useState } from "react";
+// import Header from "../../components/header/Header";
+// import Posts from "../../components/posts/Posts";
+// import Sidebar from "../../components/sidebar/Sidebar";
+// import "./home.css";
+// import axios from "axios";
+
+// const Home = () => {
+//   const [posts, setPosts] = useState([]);
+
+//   useEffect(() => {
+//     const fetchPosts = async () => {
+//       const res = await axios.get("/posts");
+//       setPosts(res.data);
+//     };
+//     fetchPosts();
+//   }, []);
+//   return (
+//     <>
+//       <Header />
+//       <div className="home">
+//         <Posts posts={posts} />
+//         <Sidebar />
+//       </div>
+//     </>
+//   );
+// };
+
+// export default Home;
